050de16ef9c4ca8f0e75c226e825a1f4069095f2
rodrigonex <46873672+rodrigonex@users.noreply.github.com>
2021-09-15 16:44:43 -0300

    delete key
    correção de falha de segurança

--- src/config/key.ts
+++
@@ -1,8 +0,0 @@
-function key() {
-  const password = '138010';
-  const login = 'rodrigoxd';
-  const uri = `mongodb+srv://${login}:${password}@cluster0.nvkja.mongodb.net/grades?retryWrites=true&w=majority`;
-  return uri;
-}
-
-export default key();
